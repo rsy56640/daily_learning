1be181f156d0904e5b34f3e301b53d3f686f087b
rsy56640 <851911672@qq.com>
2018-08-25 22:39:36 +0800

    ..

--- Trivial_practice/2018-08-23_macro_implement_sync_Cpp/main.cpp
+++ Trivial_practice/2018-08-23_macro_implement_sync_Cpp/main.cpp
@@ -66,28 +66,28 @@ namespace Sync {
 	 * SYNCHRONIZED_OBJ
 	 */
 #define SYNCHRONIZED_OBJ(obj) \
-	INJECT_VARIABLE(static std::mutex, obfuscatedMutexName) \
 	INJECT_VARIABLE(std::lock_guard<std::mutex>,            \
-						obfuscatedLockName(Sync::Synchronized_OBJ<decltype(obj)>(obj, obfuscatedMutexName).GetMutex())) 
+						obfuscatedLockName(Sync::Synchronized_OBJ<decltype(obj)>(obj).GetMutex())) 
 
 	template<class T>
 	class Synchronized_OBJ
 	{
 	public:
-		Synchronized_OBJ(T& _Synchronized_obj, std::mutex& _myMutex)
-			:Synchronized_obj(_Synchronized_obj), myMutex(_myMutex)
+		Synchronized_OBJ(T& _Synchronized_obj)
+			:Synchronized_obj(_Synchronized_obj)
 		{}
 		std::mutex& GetMutex() {
-			return myMutex;
+			return Synchronized_obj.getMutex();
 		}
 	private:
 		T & Synchronized_obj;
-		std::mutex& myMutex;
 	};
 
 	class Test {
 		int i = 0;
+		std::mutex mutex_;
 	public:
+		std::mutex& getMutex() { return mutex_; }
 		int operator++(int) {
 			int temp = i;
 			i++;
@@ -98,21 +98,32 @@ namespace Sync {
 			return os;
 		}
 	};
-	Test t;
+	Test t1, t2;
 
-	void foo_SYNCHRONIZED_OBJ(int k)
+	void foo_SYNCHRONIZED_OBJ_1(int k)
 	{
 		std::cout << "Test SYNCHRONIZED_OBJ in thread " << k << std::endl;
 		for (int i = 0; i < LOOP_TIMES; i++)
 		{
-			SYNCHRONIZED_OBJ(t)
+			SYNCHRONIZED_OBJ(t1)
 			{
-				t++;
-				std::cout << t << " in the thread: " << k << std::endl;
+				t1++;
+				std::cout << t1 << " in the thread: " << k << std::endl;
 			}
 		}
 	}
-
+	void foo_SYNCHRONIZED_OBJ_2(int k)
+	{
+		std::cout << "Test SYNCHRONIZED_OBJ in thread " << k << std::endl;
+		for (int i = 0; i < LOOP_TIMES; i++)
+		{
+			SYNCHRONIZED_OBJ(t2)
+			{
+				t2++;
+				std::cout << t2 << " in the thread: " << k << std::endl;
+			}
+		}
+	}
 
 	/*
 	 * SYNCHRONIZED_METHOD
@@ -151,13 +162,14 @@ namespace Sync {
 	 */
 #define SYNCHRONIZED_MEMBER_FUNCTION \
 	try { \
-		INJECT_VARIABLE(static std::mutex, obfuscatedMutexName) \
-			 throw std::move(std::unique_lock<std::mutex>(Sync::Synchronized_OBJ<decltype(*this)>(*this, obfuscatedMutexName).GetMutex())); \
+		throw std::move(std::unique_lock<std::mutex>(Sync::Synchronized_OBJ<decltype(*this)>(*this).GetMutex())); \
 	} catch (std::unique_lock<std::mutex>&)
 
 
 	class A {
+		std::mutex mutex_;
 	public:
+		std::mutex& getMutex() { return mutex_; }
 		void foo_SYNCHRONIZED_MEMBER_FUNCTION(int k) SYNCHRONIZED_MEMBER_FUNCTION
 		{
 			std::cout << "Test SYNCHRONIZED_MEMBER_FUNCTION in thread " << k << std::endl;
@@ -195,10 +207,14 @@ void test_SYNCHRONIZED() {
 	t2.join();
 }
 void test_SYNCHRONIZED_OBJ() {
-	std::thread t1(foo_SYNCHRONIZED_OBJ, 1);
-	std::thread t2(foo_SYNCHRONIZED_OBJ, 2);
+	std::thread t1(foo_SYNCHRONIZED_OBJ_1, 1);
+	std::thread t2(foo_SYNCHRONIZED_OBJ_1, 2);
+	std::thread t3(foo_SYNCHRONIZED_OBJ_2, 3);
+	std::thread t4(foo_SYNCHRONIZED_OBJ_2, 4);
 	t1.join();
 	t2.join();
+	t3.join();
+	t4.join();
 }
 void test_SYNCHRONIZED_METHOD() {
 	std::thread t1(foo_SYNCHRONIZED_METHOD1, 1);
@@ -211,13 +227,13 @@ void test_SYNCHRONIZED_METHOD() {
 	t4.join();
 }
 void test_SYNCHRONIZED_MEMBER_FUNCTION() {
-	A a;
+	A a1, a2;
 	auto const ____1 = [](A& a, int k) { a.foo_SYNCHRONIZED_MEMBER_FUNCTION(k); };
 	auto const ____2 = [](A& a, int k) { a.foo_SYNCHRONIZED_OBJ(k); };
-	std::thread t1(____1, std::ref(a), 1);
-	std::thread t2(____1, std::ref(a), 2);
-	std::thread t3(____2, std::ref(a), 3);
-	std::thread t4(____2, std::ref(a), 4);
+	std::thread t1(____1, std::ref(a1), 1);
+	std::thread t2(____1, std::ref(a1), 2);
+	std::thread t3(____1, std::ref(a2), 3);
+	std::thread t4(____1, std::ref(a2), 4);
 	t1.join();
 	t2.join();
 	t3.join();
